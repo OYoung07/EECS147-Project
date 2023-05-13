3e28c7be29327bee569c51820a30b3ac439b95ba
Ted O'Young <toyoung@bender.engr.ucr.edu>
2023-05-13 12:35:52 -0700

    promptUpdate

--- main.cu
+++ main.cu
@@ -6,6 +6,7 @@
 
 int main (int argc, char *argv[]) {
     Timer timer;
+    int userChoice;
    
     struct body b1;
     b1.mass = 100;
@@ -19,18 +20,25 @@ int main (int argc, char *argv[]) {
     b2.position.y = 10000;
     b2.position.z = 100;
      
-   
+    printf("Press 1 for CPU calculations or 2 for GPU calculations: ");
+    scanf("%d", &userChoice);
+    
+    //printf("\nEntered number is: "%d", userChoice);
+    
+    if ("%d", userChoice == 1) {
+        printf("You chose to calculate using the CPU\n");
+    }
+    if ("%d", userChoice == 2) {
+        printf("You chose to calculate using the GPU\n");
+    }    
+
     printf("%.15f\n",calculate_FG(&b1,&b2));
    
     float3 vec = get_direction_vector(&b1,&b2);
 
     printf("(%.5f,%.5f,%.5f)\n",vec.x,vec.y,vec.z);
 
-    float3 accel = get_accel_vector(&b1, &b2);
+    //float3 accel = get_accel_vector(&b1, &b2);
 
-    //printf("(%.20f,%.20f,%.20f)\n",accel.x,accel.y,accel.z);
-    print_float3(accel);
-    printf("\n");
- 
-    printf("haha lmao\n");
+    //printf("(%.5f,%.5f,%.5f)\n",vec.x,vec.y,vec.z);
 }
