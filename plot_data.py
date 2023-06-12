9448b5382c7b2dc5cc16c983b7f33c780bfed4eb
meslane <merrickslane@gmail.com>
2023-06-11 20:06:04 -0700

    improve plotting

--- plot_data.py
+++ plot_data.py
@@ -2,6 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import math
 
 with open('outputData.csv', newline='') as csvfile:
     data = list(csv.reader(csvfile))
@@ -17,11 +18,34 @@ for line in data:
         tick.append(int(float(line[0])))
         bodies.append(int(float(line[1])))
         mass.append(float(line[2]))
+        kinetic.append(float(line[3]))
+        potential.append(float(line[4]))
+        total.append(abs(float(line[5])))
 
 #print(data)
 
-plt.plot(tick, bodies, label='Number of bodies')
-plot.plot(tick, )
+fig = plt.figure()
+ax = fig.add_subplot(2, 1, 1)
+ax.semilogy(tick, kinetic)
+ax.semilogy(tick, potential)
+ax.semilogy(tick, total)
+ax.set_title('System Energy')
+ax.set_xlabel('Tick')
+ax.set_ylabel('Energy (J)')
+ax.grid(which='both', axis='both')
 
-plt.legend()
+ax = fig.add_subplot(2, 1, 2)
+ax.plot(tick, bodies)
+ax.set_title('Body Count')
+ax.set_xlabel('Tick')
+ax.set_ylabel('Bodies')
+ax.grid(which='both', axis='both')
+
+'''
+plt.plot(tick, bodies, label='Number of bodies')
+plt.plot(tick, kinetic, label='Kinetic Energy')
+plt.plot(tick, potential, label='Potential Energy')
+plt.plot(tick, total, label='Net Energy')
+'''
+#plt.legend()
 plt.show()
